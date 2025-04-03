4333d16c8b41aa3dc6eb5dd2f6b14b124a3cf749
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-03 10:51:36 +0530

    Initialized Images

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,14 +3,16 @@ import Link from "next/link"
 
 export const Navbar = () => {
   return (
-    <nav>
-    <div>
-        <Link href='/'>E-Cart</Link>
-    </div>
-    <div>
-        <Link href='/'>Home</Link>
-        <Link href='/products'>Products</Link>
-        <Link href='/checkout'>Checkout</Link>
+    <nav className="sticky top-0 z-50 bg-white shadow">
+    <div className="container mx-auto  flex items-center justify-between px-4 py-4">
+           <Link href='/' className="hover:text-blue-600">E-Cart</Link>
+      
+       <div className="hidden md:flex space-x-6">
+           <Link href='/'>Home</Link>
+           <Link href='/products' className="hover:text-blue-600">Products</Link>
+           <Link href='/checkout' className="hover:text-blue-600">Checkout</Link>
+       </div>
+       <div className="flex items-center space-x-4"></div>
     </div>
     </nav>
   )
