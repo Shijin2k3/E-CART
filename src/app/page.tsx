7aa02596e3fe9527782ce7f1b6945ace9afc0270
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-03 13:22:25 +0530

    Initialized Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
+import { Button } from "@/components/ui/button";
+import { stripe } from "@/lib/stripe";
+import { Link } from "lucide-react";
 import Image from "next/image";
+import { Carousel } from "./components/Carousel";
 
-export default function Home() {
+export default async function Home() {
+  //fetch products from stripe
+  const products = await stripe.products.list({
+    expand:["data.default_price"],
+    limit:5,
+  })
+  
   return (
     <div >
-      <h1>helolo</h1>
+     <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+              Welcome to My Ecommerce
+            </h2>
+            <p className="text-neutral-600">
+              Discover the latest products at the best prices.
+            </p>
+            <Button
+              asChild
+              variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >
+              <Link
+                href="/products"
+                className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse All Products
+              </Link>
+            </Button>
+          </div>
+          <Image
+            alt="Hero Image"
+            src={products.data[0].images[0]}
+            className="rounded"
+            width={450}
+            height={450}
+          />
+        </div>
+      </section>    
+      <section className="py-8">
+        <Carousel />
+      </section>
 
      
     </div>
