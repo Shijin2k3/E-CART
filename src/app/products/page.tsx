bcd29f6024b7104d5b89706e981931af87ddb4e0
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-05 10:50:18 +0530

    Initialized ProductList

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,6 @@
 import { Expand } from "lucide-react"
 import {stripe} from "@/lib/stripe"
-import { ProductList } from "../components/productList"
+import { ProductList } from "../components/ProductList"
 
 
 export default  async function ProductsPage() {
@@ -8,8 +8,8 @@ export default  async function ProductsPage() {
     expand:["data.default_price"],
   })
   return (
-    <div>
-      <h1>All Products</h1>
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">All Products</h1>
       <ProductList products={products.data}/>
     </div>
   )
