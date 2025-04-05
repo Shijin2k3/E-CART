2ef708dbe0d376ce9411c1690cc202543581bb67
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-05 11:07:23 +0530

    Initialized Search Product

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Stripe from "stripe"
 import { ProductCard } from "./ProductCard"
 import { useState } from "react"
@@ -7,19 +8,29 @@ interface Props{
 
 }
 export const ProductList =({products}:Props)=>{
-
+    // serach logic
     const[searchTerm,setSearchTerm]=useState<string>('')
+    const filterProduct=products.filter((product) => {
+        const term =searchTerm.toLowerCase()
+        const nameMatch=product.name.toLowerCase().includes(term)
+        const descriptionMatch=product.description? product.description.toLowerCase().includes(term):false
+
+        return nameMatch || descriptionMatch;
+    })
+
     return(
         <div>
 
             
             <div className="mb-6 flex justify-center">
                 <input type="text" placeholder="Search Products..."
+                value={searchTerm}
+                onChange={(e)=> setSearchTerm(e.target.value)}
                  className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
 
             </div>
             <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                {products.map((product,key)=>{
+                {filterProduct.map((product,key)=>{
                     return(
                         <li key={key}><ProductCard product={product}/></li>
                     )
